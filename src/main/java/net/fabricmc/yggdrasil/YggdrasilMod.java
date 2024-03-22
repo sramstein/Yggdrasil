a42e9cff2be31eb0a9bf81aeb9ae1bcb0373f8af
sramstein <sacha.ramstein@epitech.com>
2024-03-22 22:04:50 +0100

    Init First item, without texture

--- src/main/java/net/fabricmc/yggdrasil/YggdrasilMod.java
+++ src/main/java/net/fabricmc/yggdrasil/YggdrasilMod.java
@@ -1,6 +1,7 @@
 package net.fabricmc.yggdrasil;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.yggdrasil.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,6 +9,8 @@ public class YggdrasilMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MOD_ID = "yggdrasilmod";
+
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
 	@Override
@@ -15,7 +18,7 @@ public class YggdrasilMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		ModItems.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/net/fabricmc/yggdrasil/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.fabricmc.yggdrasil.item;
+
+import net.fabricmc.yggdrasil.YggdrasilMod;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+    public static final Item GEMSTONE_JOTUNHEIM = registerItem("gemstone_jotunheim",
+        new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    private static Item registerItem (String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(YggdrasilMod.MOD_ID, name), item);
+    }
+    public static void registerModItems() {
+        YggdrasilMod.LOGGER.info("Registering Mod for items " + YggdrasilMod.MOD_ID);
+    }
+}
