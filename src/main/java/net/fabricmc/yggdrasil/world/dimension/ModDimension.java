307723478cbdeb70cede1dfa9796583d29e7b7a1
Antoine Grand <antoine.grand@epitech.eu>
2024-03-23 16:19:06 +0100

    [+] fix the portals.

--- src/main/java/net/fabricmc/yggdrasil/world/dimension/ModDimension.java
+++ src/main/java/net/fabricmc/yggdrasil/world/dimension/ModDimension.java
@@ -5,33 +5,14 @@ import net.fabricmc.yggdrasil.item.ModItems;
 import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryKey;
-import net.minecraft.world.World;
-import net.minecraft.world.dimension.DimensionType;
 
 public class ModDimension {
-    public static final RegistryKey<World> JOTUNHEIM_DIMENSION_KEY = RegistryKey.of(Registry.WORLD_KEY,
-            new Identifier(YggdrasilMod.MOD_ID, "jotunheim"));
-    public static final RegistryKey<DimensionType> JOTUNHEIM_TYPE_KEY = RegistryKey.of(Registry.DIMENSION_TYPE_KEY,
-            JOTUNHEIM_DIMENSION_KEY.getValue());
-
-    public static final RegistryKey<World> HELHEIM_DIMENSION_KEY = RegistryKey.of(Registry.WORLD_KEY,
-            new Identifier(YggdrasilMod.MOD_ID, "helheim"));
-    public static final RegistryKey<DimensionType> HELHEIM_TYPE_KEY = RegistryKey.of(Registry.DIMENSION_TYPE_KEY,
-            HELHEIM_DIMENSION_KEY.getValue());
-
-    public static final RegistryKey<World> VALHOLL_DIMENSION_KEY = RegistryKey.of(Registry.WORLD_KEY,
-            new Identifier(YggdrasilMod.MOD_ID, "valholl"));
-    public static final RegistryKey<DimensionType> VALHOLL_TYPE_KEY = RegistryKey.of(Registry.DIMENSION_TYPE_KEY,
-            VALHOLL_DIMENSION_KEY.getValue());
-
     public static void register() {
         YggdrasilMod.LOGGER.debug("Registering ModDimension for " + YggdrasilMod.MOD_ID);
 
         CustomPortalBuilder.beginPortal()
                 .frameBlock(Blocks.ICE)
-                .destDimID(JOTUNHEIM_DIMENSION_KEY.getValue())
+                .destDimID(new Identifier("yggdrasil", "jotunheim"))
                 .tintColor(153, 204, 255)
                 .lightWithItem(ModItems.JOTUNHEIM_GEMSTONE)
                 .onlyLightInOverworld()
@@ -39,7 +20,7 @@ public class ModDimension {
 
         CustomPortalBuilder.beginPortal()
                 .frameBlock(Blocks.NETHERRACK)
-                .destDimID(HELHEIM_DIMENSION_KEY.getValue())
+                .destDimID(new Identifier("yggdrasil", "helheim"))
                 .tintColor(255, 51, 51)
                 .lightWithItem(ModItems.HELHEIM_GEMSTONE)
                 .onlyLightInOverworld()
@@ -47,7 +28,7 @@ public class ModDimension {
 
         CustomPortalBuilder.beginPortal()
                 .frameBlock(Blocks.GLOWSTONE)
-                .destDimID(VALHOLL_DIMENSION_KEY.getValue())
+                .destDimID(new Identifier("yggdrasil", "valholl"))
                 .tintColor(224, 224, 224)
                 .lightWithItem(ModItems.VALHOLL_GEMSTONE)
                 .onlyLightInOverworld()
