e58155817d9429ba77355671ad75c353d56db1af
Antoine Grand <antoine.grand@epitech.eu>
2024-03-24 22:44:27 +0100

    [+] add the generation of the mithril in the jotunheim universe.

--- src/main/java/net/fabricmc/yggdrasil/world/feature/ModConfiguredFeatures.java
+++ src/main/java/net/fabricmc/yggdrasil/world/feature/ModConfiguredFeatures.java
@@ -9,15 +9,11 @@ import java.util.List;
 
 public class ModConfiguredFeature {
     public static final List<OreFeatureConfig.Target> JOTUNHEIM_MITHRIL_ORE = List.of(
-            OreFeatureConfig.createTarget(OreConfiguredFeatures.STONE_ORE_REPLACEABLES,
-                    ModBlocks.MITHRIL_ORE.getDefaultState()),
-            OreFeatureConfig.createTarget(OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES,
-                    ModBlocks.DEEPSLATE_MITHRIL_ORE.getDefaultState())
-    );
+            OreFeatureConfig.createTarget(OreConfiguredFeatures.STONE_ORE_REPLACEABLES, ModBlocks.MITHRIL_ORE.getDefaultState()),
+            OreFeatureConfig.createTarget(OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.DEEPSLATE_MITHRIL_ORE.getDefaultState()));
 
     public static final RegistryEntry<ConfiguredFeature<OreFeatureConfig, ?>> MITHRIL_ORE =
-            ConfiguredFeatures.register("mythril_ore", Feature.ORE,
-                    new OreFeatureConfig(JOTUNHEIM_MITHRIL_ORE, 9));
+            ConfiguredFeatures.register("mythril_ore", Feature.ORE, new OreFeatureConfig(JOTUNHEIM_MITHRIL_ORE, 9));
 
     public static void registerCongiguredFeatures() {
         YggdrasilMod.LOGGER.info("Registering ModConfiguredFeatures for " + YggdrasilMod.MOD_ID);
