bde654b9cf73cca293910256f89b3d98babc4a85
Corent1P <corentin.piquet@epitech.eu>
2024-03-24 23:27:51 +0100

    [+] Add Yggdrasil tree generation

--- src/main/java/net/fabricmc/yggdrasil/world/structure/ModStructures.java
+++ src/main/java/net/fabricmc/yggdrasil/world/structure/ModStructures.java
@@ -15,12 +15,9 @@ public class ModStructures {
      * It is always a good idea to register your Structures so that other mods and datapacks can
      * use them too directly from the registries. It great for mod/datapacks compatibility.
      */
-    public static StructureFeature<?> SKY_STRUCTURES = new SkyStructures();
 
     public static void registerStructureFeatures() {
         // The generation step for when to generate the structure. there are 10 stages you can pick from!
-        // This surface structure stage places the structure before plants and ores are generated.
-        StructureFeatureAccessor.callRegister("yggdrasil:sky_structures",
-                SKY_STRUCTURES, GenerationStep.Feature.SURFACE_STRUCTURES);
+        // This surface structure stage places the structure before plants and ores are generated
     }
 }
